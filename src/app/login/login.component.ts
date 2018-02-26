053047d1450493677229bd0ba6ffe7898213cc8b
thianlopezz <thianlopezz@gmail.com>
2018-02-26 12:50:49 -0500

    responsive, login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var jQuery: any;
+declare var Materialize: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+
+    jQuery('select').material_select();
+    jQuery('ul.tabs').tabs();
+    jQuery('.collapsible').collapsible();
+    jQuery('.modal').modal();
   }
 
 }
